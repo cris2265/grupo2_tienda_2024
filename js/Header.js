f26f38f1317b459c90054a57c5b95d1d5f06d5f6
DereckAbraham <dacarranza@scl.edu.gt>
2024-05-08 13:34:14 -0600

    Header - Dacarranza

---
+++ js/Header.js
@@ -0,0 +1,24 @@
+let header = document.querySelector("#header")
+header.innerHTML = `
+
+<nav>
+    <nav class="logito">
+        <a href="https://github.com/DereckAbraham" class="logo-link">
+            <img src="https://i0.wp.com/www.followchain.org/wp-content/uploads/2024/02/sad-hamster-Photoroom.png-Photoroom.png?w=604&ssl=1" alt="Logo" class="log">
+            <h2>Chicharrones Shop</h2>
+        </a>
+    </nav>
+    
+    <nav class="tipos">
+        <div>Home</div>
+        <div>Products</div>
+        <div>About</div>
+        <div>Support</div>
+    </nav>
+    <nav class="imagen">
+        <img src="https://i0.wp.com/www.followchain.org/wp-content/uploads/2024/02/sad-hamster-Photoroom.png-Photoroom.png?w=604&ssl=1" alt="" class="log">
+        <img src="https://i0.wp.com/www.followchain.org/wp-content/uploads/2024/02/sad-hamster-Photoroom.png-Photoroom.png?w=604&ssl=1" alt="" class="logDiferente">
+    </nav>
+</nav>
+
+`
