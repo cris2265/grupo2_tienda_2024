546197c5a56123a7826b161ec197a468d583e38e
cris2265 <cehernandez@scl.edu.gt>
2024-05-08 13:33:15 -0600

    Footer

---
+++ js/Footer.js
@@ -0,0 +1,37 @@
+let footer = document.querySelector("#footer");
+
+footer.innerHTML = `
+    <div id="secciones_1">
+        <h3>Tienda</h3>
+        <span>Get started noew try our product</span>
+        <div id="mail">
+            <input type="email" placeholder="Enter your email here">
+            <div id="flechita"></div>
+        </div>
+        <span id="copy">© 2024 Tienda Chicharronera.inc. Copyrigth and rights reserved</span>
+    </div>
+    <div id="secciones_2">
+        <div id="suport_1">
+            <span id="titulo">Support</span>
+            <span id="subtitulos">Help centre</span>
+            <span id="subtitulos">Account information</span>
+            <span id="subtitulos">About</span>
+            <span id="subtitulos">Contact us</span>
+        </div>
+        <div id="suport_2">
+            <span id="titulo">Help and Solution</span>
+            <span id="subtitulos">Talk to support</span>
+            <span id="subtitulos">Support docs</span>
+            <span id="subtitulos">System status</span>
+            <span id="subtitulos">Covid responde</span>
+        </div>
+        <div id="suport_3">
+            <span id="titulo">Product</span>
+            <span id="subtitulos">Update</span>
+            <span id="subtitulos">Security</span>
+            <span id="subtitulos">Beta test</span>
+            <span id="subtitulos">Pricing product</span>
+        </div>
+        <h3>Terms and Conditions * Privacy Policy</h3>
+    </div>
+`
